d7ec60c8800ecd81e91179fe7437e891ce7b30ca
Crowdbotics <team@crowdbotics.com>
2021-08-10 00:39:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sweet_darkness_29683Identifier",
+  url: "https://sweet_darkness_29683Identifier.botics.co",
+  api: "https://sweet_darkness_29683Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
